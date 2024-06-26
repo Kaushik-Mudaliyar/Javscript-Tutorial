022ede89fe74ee15033523f28a357e6d9d5559fe
Kaushik <140623856+Kaushik-Mudaliyar@users.noreply.github.com>
2024-06-26 08:34:54 +0000

    Completed variables in javascript

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 1435;
+let accountEmail = "Kaushik@google.com";
+var accountPassword = "12345";
+let accountState;
+accountCity = "Kalol";
+
+accountEmail="Manish@google.com"
+accountPassword=233
+accountCity="Ahemdabad"
+
+// accountId=3  It is not allowed in javascript
+
+//Prefer not to use var because of issue in block scope and functional scope
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity])
+console.log(accountState)
