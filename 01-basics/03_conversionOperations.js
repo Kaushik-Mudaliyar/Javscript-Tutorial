6122e49c61470d089c6ce889c6fc6fe829df1168
Kaushik <140623856+Kaushik-Mudaliyar@users.noreply.github.com>
2024-06-26 11:08:13 +0000

    Conversion operations in js

---
+++ 01-basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let score ="Kaushik"
+// console.log(typeof score);
+// console.log(score);
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+//Converted to boolean using Boolean function
+// 1 => true ; 0 => false
+//" " => false; "Kaushik"(string) => true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
