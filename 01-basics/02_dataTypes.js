8c748accf3d2de61338e6fbc3577b2b91a9e8f4a
Kaushik <140623856+Kaushik-Mudaliyar@users.noreply.github.com>
2024-06-26 10:54:33 +0000

    datatypes

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //Treat all code as newer version of javascript
+//In todays time we don't need to declare it, it automatically treat all the code in newer version.
+
+// alert("Hello") we can use it in browser, but we are using the node.js so it would not run and throws error
+
+console.log(3+3)
+console.log("Kaushik")
+
+
+let name="kaushik"
+let age=20
+let isLoggedIn=true
+
+
+//DataTypes in Javascript:-
+//number 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null
+//undefined
+//symbol => mostly used in unique
+//Object
+
+
+console.log(typeof "kaushik")
+console.log(typeof age)
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
+console.log(typeof isLoggedIn)
