ceac6e4c5093b13ec19c1ef42154a6341b857cef
Kaushik <140623856+Kaushik-Mudaliyar@users.noreply.github.com>
2024-06-28 07:51:29 +0000

    Strings and String Methods in javascript

---
+++ 01-basics/05_String.js
@@ -0,0 +1,34 @@
+const name="Kaushik"
+const repoCount=50
+
+// In todays time don't write the code like this becuase it is outdated...
+// console.log(name + repoCount)
+
+//Otherwise write it like these:-(using string interpolation)
+console.log(`Hello My name is ${name} and my repo Count is ${repoCount}`)
+
+// You can also create the string using new keyword
+const gameName = new String(`Kaushik  ms`)
+console.log(gameName);
+
+// Methods of String in javascript
+// we can access single word through the below context:
+// console.log(gameName[0]);
+
+// console.log(gameName.concat(" is best"));
+// console.log(gameName.charAt(0));
+// console.log(gameName.length);
+// console.log(gameName.indexOf("k"));
+// console.log(gameName.endsWith("s"));
+// console.log(gameName.startsWith("K"));
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.replace("ms","Op"));
+// console.log(gameName.substring(1,4));
+// console.log(gameName.trim( ));
+// console.log(gameName.slice(1,4));
+
+const url="https://kaushik.com/kaushik%20mudaliyar"
+// console.log(url.replace(`%20`,`-`))
+
+
