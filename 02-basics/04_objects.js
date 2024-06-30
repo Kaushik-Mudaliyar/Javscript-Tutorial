e9bbc8d4047035393bad05fa3ed40415d36148e1
Kaushik <140623856+Kaushik-Mudaliyar@users.noreply.github.com>
2024-06-30 09:26:48 +0000

    More about objects

--- 02-basics/04_objects.js
+++ 02-basics/04_objects.js
@@ -21,3 +21,41 @@ const regularUser ={
 
 // console.log(regularUser.fullname?.userfullname);
 
+// Here we are taking two objects such that we need to concat the values of both obj1 and obj2..
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"c",4:"d"}
+const obj3={5:"e",6:"f"}
+// const obj3={obj1,obj2}
+// So here inside the obj3 there is obj1 whole object and obj2 whole object but we want to enter only the values inside the two objects..
+// console.log(obj3);
+
+//Here if you want to concat two object such obj1 and obj2 we can use the assign method to concat the sources into a single object..
+// const obj4=Object.assign({},obj1,obj2,obj3);
+
+//this was the most used syntax to concat the objects using the spread method here using ... triple dots we can assign a objects value to other object..
+const obj4={...obj1,...obj2}
+console.log(obj4);
+
+// Array of objects :-
+// Here we are creating an array consists of objects and learning how to get the value of the array of object..
+const users=[
+    {
+
+    },
+    {
+
+    },
+    {
+        userid:1,
+        email:"k@google.com"
+    }
+]
+
+console.log(users[2].email)
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+// It is used to check whether the property is defind inside the object or not..
+console.log(tinderUser.hasOwnProperty(`isLoggedI`));
