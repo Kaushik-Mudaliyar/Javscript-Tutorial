99e1e849268385096b7a749d91af2b0d39e5ef14
Kaushik <140623856+Kaushik-Mudaliyar@users.noreply.github.com>
2024-07-01 07:34:26 +0000

    Learned about API

--- 02-basics/04_objects.js
+++ 02-basics/04_objects.js
@@ -2,30 +2,30 @@
 // const tinderUser = new Object()
 // console.log(tinderUser)
 
-const tinderUser={}
-tinderUser.id="123abc"
-tinderUser.name="Kaushik"
-tinderUser.isLoggedIn=true
+const tinderUser = {};
+tinderUser.id = "123abc";
+tinderUser.name = "Kaushik";
+tinderUser.isLoggedIn = true;
 
 // console.log(tinderUser)
 
-const regularUser ={
-    email:"some@gmail.com",
-    fullname:{
-        userfullname:{
-            firstname:"Kaushik",
-            lastname:"Mudaliyar"
-        }
-    }
-}
+const regularUser = {
+  email: "some@gmail.com",
+  fullname: {
+    userfullname: {
+      firstname: "Kaushik",
+      lastname: "Mudaliyar",
+    },
+  },
+};
 
 // console.log(regularUser.fullname?.userfullname);
 
 // Here we are taking two objects such that we need to concat the values of both obj1 and obj2..
 
-const obj1={1:"a",2:"b"}
-const obj2={3:"c",4:"d"}
-const obj3={5:"e",6:"f"}
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "c", 4: "d" };
+const obj3 = { 5: "e", 6: "f" };
 // const obj3={obj1,obj2}
 // So here inside the obj3 there is obj1 whole object and obj2 whole object but we want to enter only the values inside the two objects..
 // console.log(obj3);
@@ -34,23 +34,19 @@ const obj3={5:"e",6:"f"}
 // const obj4=Object.assign({},obj1,obj2,obj3);
 
 //this was the most used syntax to concat the objects using the spread method here using ... triple dots we can assign a objects value to other object..
-const obj4={...obj1,...obj2}
+const obj4 = { ...obj1, ...obj2 };
 // console.log(obj4);
 
 // Array of objects :-
 // Here we are creating an array consists of objects and learning how to get the value of the array of object..
-const users=[
-    {
-
-    },
-    {
-
-    },
-    {
-        userid:1,
-        email:"k@google.com"
-    }
-]
+const users = [
+  {},
+  {},
+  {
+    userid: 1,
+    email: "k@google.com",
+  },
+];
 
 // console.log(users[2].email)
 // console.log(tinderUser);
@@ -60,14 +56,14 @@ const users=[
 // It is used to check whether the property is defind inside the object or not..
 // console.log(tinderUser.hasOwnProperty(`isLoggedI`));
 
-// HEre we are making an object which have keys and values 
-const course={
-    name:"Kaushik",
-    price:"999",
-    courseInstructor:"Hitesh"
-}
+// HEre we are making an object which have keys and values
+const course = {
+  name: "Kaushik",
+  price: "999",
+  courseInstructor: "Hitesh",
+};
 // So if you need to access the objects value you are using . operation to console the value like this:
-console.log(course.courseInstructor)
+console.log(course.courseInstructor);
 
 // If you want to write a clean code you can write in this way also:
 // const {courseInstructor}=course;
@@ -75,5 +71,23 @@ console.log(course.courseInstructor)
 // console.log(courseInstructor);
 
 // if you want that courseInstructor was a long name so you can give a new name to this syntax:
-const {courseInstructor:Instructor}=course;
+// You can also destructor the array also..
+// It is destructoring of the object.
+const { courseInstructor: Instructor, price } = course;
 console.log(Instructor);
+console.log(price);
+
+// Api means Application programming interface
+// Api can be give two different types of values through object or in array type also.so we can fetch through array or object values of api..
+// json
+// {
+//     "name":"Kaushik",
+//     "courseName":"Javscript tutorial"
+//     "price":"free"
+// }
+// Api in the form of array:
+// [
+//     {},
+//     {},
+//     {},
+// ]
