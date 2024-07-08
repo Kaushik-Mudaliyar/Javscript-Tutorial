56c2550d6b947bf72714d471a75ac6828fd6cff1
Kaushik <140623856+Kaushik-Mudaliyar@users.noreply.github.com>
2024-07-08 13:58:17 +0000

    switch in javascript

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,44 @@
+let month = 8;
+// This statement takes the value in terms of key and match the key with every case such that if it matches the case it prints that statement in the terminal. It is clean as if we want the same code with the help of if-else.
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("February");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  case 5:
+    console.log("May");
+    break;
+  case 6:
+    console.log("June");
+    break;
+  case 7:
+    console.log("July");
+    break;
+  case 8:
+    console.log("August");
+    break;
+  case 9:
+    console.log("September");
+    break;
+  case 10:
+    console.log("October");
+    break;
+  case 11:
+    console.log("November");
+    break;
+  case 12:
+    console.log("December");
+    break;
+
+  default:
+    console.log("Invalid Input");
+    break;
+}
